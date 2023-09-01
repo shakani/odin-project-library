d635ccb5f2263c553dabff2e42cf0e6c0a357df9
Sami Hakani <sami.hakani@gmail.com>
2023-09-01 16:06:09 -0700

    Created method to add books (3) to library

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
-    info = () => {
+    this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? "read" : "not read yet"}`;
     }
 }
 
+function addToLibrary(bookToAdd) {
+    myLibrary.push(bookToAdd);
+}
+
 theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+goodOmens = new Book("Good Omens", "Neil Gaiman", 400, false);
+recursion = new Book("Recursion", "That One Guy", 500, true);
+
+addToLibrary(theHobbit);
+addToLibrary(goodOmens);
+addToLibrary(recursion);
+
+for(let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i].info());
+}
